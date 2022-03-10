ccf566a47c193baf530b297f5c1cdeb53aae4181
Piotr Olszewski <asmie@asmie.pl>
2022-03-10 23:08:28 +0100

    Defined sender's packets' models.

---
+++ src/sender/packets.rs
@@ -0,0 +1,20 @@
+
+#[repr(packed)]
+struct UnauthenticatedPacketSender
+{
+    sequence_number: u32,
+    timestamp: u64,
+    error_estimate: u16,
+    mbz: [u8; 30],
+}
+
+#[repr(packed)]
+struct AuthenticatedPacketSender
+{
+    sequence_number: u32,
+    mbz: [u8; 12],
+    timestamp: u64,
+    error_estimate: u16,
+    mbz: [u8; 70],
+    hmac: [u8; 16],
+}
