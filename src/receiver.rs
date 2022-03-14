ea40bd8bf1c388160105420844ca9079a7f32c57
Piotr Olszewski <asmie@asmie.pl>
2022-03-14 12:31:46 +0100

    Working simple UDP connection to exchange bytes (sender and receiver). Both sender and receiver are put into separate threads.

---
+++ src/receiver.rs
@@ -0,0 +1,21 @@
+use crate::configuration::*;
+use std::sync::Arc;
+use std::net::UdpSocket;
+use std::io;
+
+pub fn receive_worker(conf :Arc<Configuration>) {
+    let socket = UdpSocket::bind((conf.local_addr, conf.local_port)).expect("Cannot bind to address");
+
+    loop {
+        let mut buf = [0u8; 65535];
+
+        let (num_bytes_read, _) = loop {
+            match socket.recv_from(&mut buf) {
+                Ok(n) => break n,
+                Err(e) => panic!("encountered IO error: {}", e),
+            }
+        };
+
+        println!("bytes: {:?}", &buf[..num_bytes_read]);
+    }
+}
--- src/sender.rs
+++ src/sender.rs
@@ -1,8 +1,14 @@
 use crate::configuration::*;
 use std::sync::Arc;
+use std::net::UdpSocket;
+use std::io;
 
 pub fn sender_worker(conf :Arc<Configuration>) {
+    let socket = UdpSocket::bind("0.0.0.0:0").expect("Cannot bind to address");
+    socket.connect((conf.remote_addr.unwrap(), conf.remote_port)).expect("Cannot connect to address");
 
+    socket.send(&[0,1,2,3,4,5,6,7]);
+    socket.send(&[10,9,2,3,4,5,6,7]);
 }
 
 
