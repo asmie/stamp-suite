87afd46ec5e1037489df6cb7fda3f11d53818802
Piotr Olszewski <asmie@asmie.pl>
2022-03-28 00:09:46 +0200

    Correction to the NTP fraction calculations.

--- src/time.rs
+++ src/time.rs
@@ -15,7 +15,7 @@ pub fn generate_timestamp(cs : ClockFormat) -> u64 {
 
 fn convert_dt_to_ntp(date : DateTime<Utc>) -> u64 {
     let secs = (date.timestamp() + NTP_UNIX_OFFSET) as u32;
-    let fraction = date.timestamp_subsec_millis() / u32::MAX / 1000;
+    let fraction = ((date.timestamp_subsec_millis() as u64) * u32::MAX as u64 / 1000u64) as u32;
 
     ((secs as u64) << 32) | fraction as u64
 }
@@ -32,17 +32,37 @@ mod tests {
 
     #[test]
     fn convert_dt_to_ntp_test() {
-        let sample = DateTime::<Utc>::from_utc(NaiveDateTime::from_timestamp(1525987, 0), Utc);
-        let test_val = convert_dt_to_ntp(sample);
-
+        let mut sample = DateTime::<Utc>::from_utc(NaiveDateTime::from_timestamp(1525987, 0), Utc);
+        let mut test_val = convert_dt_to_ntp(sample);
         assert_eq!(sample.timestamp(), (test_val >> 32) as i64 - NTP_UNIX_OFFSET);
+        assert_eq!(sample.timestamp_subsec_millis(), (test_val as u32) / u32::MAX * 1000 );
+
+        sample = DateTime::<Utc>::from_utc(NaiveDateTime::from_timestamp(0, 0), Utc);
+        test_val = convert_dt_to_ntp(sample);
+        assert_eq!(sample.timestamp(), (test_val >> 32) as i64 - NTP_UNIX_OFFSET);
+        assert_eq!(sample.timestamp_subsec_millis(), (test_val as u32) / u32::MAX * 1000 );
+
+        sample = DateTime::<Utc>::from_utc(NaiveDateTime::from_timestamp(2584229, 151000000), Utc);
+        test_val = convert_dt_to_ntp(sample);
+        assert_eq!(sample.timestamp(), (test_val >> 32) as i64 - NTP_UNIX_OFFSET);
+        assert_eq!(sample.timestamp_subsec_millis(), ((test_val as u32) as u64 * 1000u64 / u32::MAX as u64 ) as u32 + 1);
     }
 
     #[test]
     fn convert_dt_to_ptp_test() {
-        let sample = DateTime::<Utc>::from_utc(NaiveDateTime::from_timestamp(1525987, 0), Utc);
-        let test_val = convert_dt_to_ptp(sample);
-
+        let mut sample = DateTime::<Utc>::from_utc(NaiveDateTime::from_timestamp(1525987, 0), Utc);
+        let mut test_val = convert_dt_to_ptp(sample);
         assert_eq!(sample.timestamp(), (test_val >> 32) as i64);
+        assert_eq!(sample.timestamp_subsec_nanos(), test_val as u32);
+
+        sample = DateTime::<Utc>::from_utc(NaiveDateTime::from_timestamp(0, 0), Utc);
+        test_val = convert_dt_to_ptp(sample);
+        assert_eq!(sample.timestamp(), (test_val >> 32) as i64);
+        assert_eq!(sample.timestamp_subsec_nanos(), test_val as u32);
+
+        sample = DateTime::<Utc>::from_utc(NaiveDateTime::from_timestamp(2584229, 25003600), Utc);
+        test_val = convert_dt_to_ptp(sample);
+        assert_eq!(sample.timestamp(), (test_val >> 32) as i64);
+        assert_eq!(sample.timestamp_subsec_nanos(), test_val as u32);
     }
 }
