24aed515f279d6235ff85bf80f2b8d2317529d6a
Piotr Olszewski <asmie@asmie.pl>
2022-04-12 11:47:24 +0200

    First approach to raw sockets.

--- src/bin/stampd.rs
+++ src/bin/stampd.rs
@@ -2,7 +2,7 @@
 extern crate log;
 
 use std::borrow::Borrow;
-use stamp_suite::{configuration, packets, sender, receiver, packets::*};
+use stamp_suite::{configuration, packets, sender, receiver::*, packets::*};
 use crate::configuration::*;
 
 use std::thread;
@@ -11,8 +11,11 @@ use std::io::Read;
 use std::mem;
 use std::slice;
 use std::io;
-
-
+use stamp_suite::time::generate_timestamp;
+//use std::os::unix::io::AsRawFd;
+//use nix::sys::socket;
+//use nix::sys::socket::setsockopt;
+//use nix::sys::socket::sockopt;
 
 fn main()
 {
@@ -25,8 +28,6 @@ fn main()
 
     let worker = thread::spawn(move || worker(args));
 
-    // Now we need to set up the communication channels and implement high-level logic.
-
     worker.join();
 }
 
@@ -36,42 +37,27 @@ type BufferType = [u8; 65535];
 fn worker(conf : Configuration)
 {
     let socket = UdpSocket::bind((conf.local_addr, conf.local_port)).expect("Cannot bind to address");
+    //let raw_fd: RawFd = socket.as_raw_fd();
+
+    //setsockopt(raw_fd, sockopt::IpRecvErr, &true).expect("sockopt failed");
+    //setsockopt(raw_fd, sockopt::IpRecvTtl, &true).expect("sockopt failed");
+    //setsockopt(raw_fd, sockopt::IpMtuDiscover, &true).expect("sockopt failed");
 
     loop {
         let mut buf : BufferType = [0u8; 65535];
 
-        let (num_bytes_read, src_addr) = loop {
-            match socket.recv_from(&mut buf) {
-                Ok(n) => break n,
-                Err(e) => panic!("encountered IO error: {}", e),
-            }
-        };
+        let (num_bytes_read, src_addr) = socket.recv_from(&mut buf).expect("socket error!");
+        let rcv_timestamp = generate_timestamp(conf.clock_source);
 
         println!("bytes: {:?}", &buf[..num_bytes_read]);
 
         let mut packet = read_struct::<PacketUnauthenticated, &[u8]>(&mut buf).unwrap();
 
-        let mut packet_resp = ReflectedPacketUnauthenticated {
-            sess_sender_timestamp: packet.timestamp,
-            sess_sender_err_estimate: packet.error_estimate,
-            sess_sender_seq_number: packet.sequence_number,
-            sess_sender_ttl: 0,
-            sequence_number: packet.sequence_number,
-            error_estimate: packet.error_estimate,
-            timestamp:0,
-            receive_timestamp: 0,
-            mbz1: 0,
-            mbz2: 0,
-            mbz3a: 0,
-            mbz3b: 0,
-        };
+        let mut packet_resp = assemble_unauth_answer(&packet, conf.clock_source, rcv_timestamp);
 
         let buf_resp = unsafe { any_as_u8_slice::<ReflectedPacketUnauthenticated>(&packet_resp) };
 
         socket.send_to(buf_resp, src_addr);
-
-
-
     }
 }
 
--- src/receiver.rs
+++ src/receiver.rs
@@ -1,4 +1,41 @@
-use crate::configuration::*;
-use std::sync::Arc;
-use std::net::UdpSocket;
 use std::io;
+use std::net::{IpAddr, UdpSocket};
+use crate::configuration::*;
+use crate::time::generate_timestamp;
+use crate::packets::{PacketUnauthenticated, ReflectedPacketUnauthenticated};
+
+pub struct HopResult {
+    src_addr: Option<IpAddr>,
+    ttl: Option<u8>,
+    data: Vec<u8>,
+}
+
+type BufferType = [u8; 65535];
+/*
+pub fn recv_message(sock: &UdpSocket) -> Result<Box<HopResult>, io::Error>
+{
+    let raw_fd: RawFd = sock.as_raw_fd();
+    let mut buf : BufferType = [0u8; 65535];
+
+    let mut packet = HopResult { 0; 0; 0;};
+    Ok()
+}
+*/
+pub fn assemble_unauth_answer(packet: &PacketUnauthenticated, cs: ClockFormat, rcvt: u64) -> ReflectedPacketUnauthenticated {
+    let mut packet_resp = ReflectedPacketUnauthenticated {
+        sess_sender_timestamp: packet.timestamp,
+        sess_sender_err_estimate: packet.error_estimate,
+        sess_sender_seq_number: packet.sequence_number,
+        sess_sender_ttl: 0,
+        sequence_number: packet.sequence_number,
+        error_estimate: packet.error_estimate,
+        timestamp: generate_timestamp(cs),
+        receive_timestamp: rcvt,
+        mbz1: 0,
+        mbz2: 0,
+        mbz3a: 0,
+        mbz3b: 0,
+    };
+
+    packet_resp
+}
